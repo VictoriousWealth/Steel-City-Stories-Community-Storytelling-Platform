5a03e0cbfad43736f518e7abfa02e64d0a49fdd7
James Godfrey <jgodfrey2@sheffield.ac.uk>
2024-02-20 14:51:11 +0000

    added first controller and couple of basic views for login to get something running

--- Steel-city/src/controllers/general.rb
+++ Steel-city/src/controllers/general.rb
@@ -1,2 +1,29 @@
+require "sinatra"
+
 get "/" do
-    
+    erb :home
+end
+
+get "/login" do
+    erb :login_Page
+end
+
+post "/login" do
+    @username = params.fetch("username", "")
+    @password = params.fetch("password", "")
+    @error = nil
+  
+    if @username == "user" && @password == "pass"
+      session["logged_in"] = true
+      redirect "/"
+    else
+      @error = "Username/Password combination incorrect"
+    end
+  
+    erb :login
+end
+
+get "/logout" do
+    session.clear
+    erb :logout
+end
