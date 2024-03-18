38a453cc574276a89d17f8e25823806794e56ab9
Adam Bentley <abentley5@sheffield.ac.uk>
2024-03-18 15:06:29 +0000

    Fixed create account validation

--- Steel-city/src/controllers/general.rb
+++ Steel-city/src/controllers/general.rb
@@ -25,34 +25,50 @@ post "/create-account" do
     Date.parse(@dob)
     @email = params.fetch("email","")
     @account_type = params.fetch("account_type","")
-    sql = "SELECT * FROM users WHERE email = ? LIMIT 1"
-    check_email = db.execute(sql,@email)
-    sql = "SELECT * FROM users WHERE username = ? LIMIT 1"
-    check_username = db.execute(sql, @username)
-    if @password!=@confirm_password
-      @error="Passwords do not match"
-    elsif check_username
-      @error="Username already in use"
-    elsif check_email
-      @error="Email already in use"
-    elsif @dob<Date.today-15*365.25
-      @error="Invalid Date of Birth"
-    else
-      user=User.new
-      numusers=User.all.count()
-      user.userid=numusers+1
-      user.username=@username
-      user.password=@password
-      user.email=@email
-      user.dateofbirth=@dob
-      user.type=@account_type
-      user.premium=0
-      user.popcorns=0
-      user.activediscount=1
-      user.save_changes
-      redirect "/"
-    end
     @error = nil
+    begin
+      db = SQLite3::Database.new 'database.sqlite3'
+      sql = "SELECT * FROM users WHERE email = ? LIMIT 1"
+      check_email = db.execute(sql,@email)
+      sql = "SELECT * FROM users WHERE username = ? LIMIT 1"
+      check_username = db.execute(sql, @username)
+      if @password!=@confirm_password
+        @error="Passwords do not match"
+      elsif !check_username.empty?
+        @error="Username already in use"
+      elsif !check_email.empty?
+        @error="Email already in use"
+      elsif @dob.empty? || Date.parse(@dob) > Date.today - (15 * 365.25)
+        @error="Invalid Date of Birth"
+      else
+        user=User.new
+        numusers=User.all.count()
+        user.userid=numusers+1
+        user.username=@username
+        user.password=@password
+        user.email=@email
+        user.dateofbirth=@dob
+        user.type=@account_type
+        user.premium=0
+        user.popcorns=0
+        user.activediscount=1
+        user.save_changes
+        session["logged_in"] = true
+        if @account_type=="reader" then
+          session["reader"] = true
+        elsif @account_type=="writer" then
+          session["writer"] = true
+        elsif @account_type=="staff" then
+          session["staff"] = true
+        end
+        redirect "/"
+      end
+    rescue SQLite3::Exception => e
+      @error = "Database error: #{e.message}"
+    ensure
+      db.close if db
+    end
+    erb :create_account
 end
 
 post "/login" do
