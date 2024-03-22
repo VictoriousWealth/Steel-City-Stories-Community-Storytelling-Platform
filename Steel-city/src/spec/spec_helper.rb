cedacf25c7cbb2c61f0fa2a1b7974ea4d6aa435b
Adam Bentley <abentley5@sheffield.ac.uk>
2024-03-22 13:24:44 +0000

    Fixed test interaction with database

--- Steel-city/src/spec/spec_helper.rb
+++ Steel-city/src/spec/spec_helper.rb
@@ -23,6 +23,6 @@ RSpec.configure do |config|
 
   # before each test is run, delete all non-hardcoded records in the Player table
   config.before do
-    User.where { account_number > 3 }.delete
+    User.where { userid > 3 }.delete
   end
 end
